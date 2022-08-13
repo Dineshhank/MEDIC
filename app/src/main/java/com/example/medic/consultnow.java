b8fe022b4b12d86f8e3900bb1ade2e843c755d4f
Dineshhank <babud2000@gmail.com>
2022-08-13 15:32:31 +0530

    consultpage

--- app/src/main/java/com/example/medic/consultnow.java
+++ app/src/main/java/com/example/medic/consultnow.java
@@ -3,12 +3,34 @@ package com.example.medic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class consultnow extends AppCompatActivity {
-
+    ViewFlipper vflipper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultnow);
+
+        int images[] ={R.drawable.img1, R.drawable.medi1, R.drawable.loginbg};
+        vflipper = findViewById(R.id.flipper);
+
+        for(int image: images){
+            flipperImages(image);
+        }
+
+    }
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        vflipper.addView(imageView);
+        vflipper.setFlipInterval(3000);
+        vflipper.setAutoStart(true);
+
+        vflipper.setInAnimation(this, android.R.anim.slide_in_left);
+        vflipper.setOutAnimation(this, android.R.anim.slide_out_right);
+
     }
 }
--- app/src/main/java/com/example/medic/general.java
+++ app/src/main/java/com/example/medic/general.java
@@ -2,13 +2,79 @@ package com.example.medic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class general extends AppCompatActivity {
-
+ImageView imgcovid,imgphy,imggyno,imgpedi,imguro,imgent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general);
+
+        this.imgcovid = (findViewById(R.id.imageView));
+        this.imgphy = (findViewById(R.id.imageView2));
+        this.imggyno = (findViewById(R.id.imageView3));
+        this.imgpedi = (findViewById(R.id.imageView4));
+        this.imguro = (findViewById(R.id.imageView5));
+        this.imgent = (findViewById(R.id.imageView6));
+
+
+        imgcovid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),consultnow.class);
+                intent.putExtra("pass","Covid Consultation");
+                general.this.startActivity(intent);
+            }
+        });
+
+        imgphy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),consultnow.class);
+                intent.putExtra("pass","Covid Consultation");
+                general.this.startActivity(intent);
+            }
+        });
+
+        imggyno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),consultnow.class);
+                intent.putExtra("pass","Covid Consultation");
+                general.this.startActivity(intent);
+            }
+        });
+
+        imgpedi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),consultnow.class);
+                intent.putExtra("pass","Covid Consultation");
+                general.this.startActivity(intent);
+            }
+        });
+
+        imguro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),consultnow.class);
+                intent.putExtra("pass","Covid Consultation");
+                general.this.startActivity(intent);
+            }
+        });
+
+        imgent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),consultnow.class);
+                intent.putExtra("pass","Covid Consultation");
+                general.this.startActivity(intent);
+            }
+        });
     }
 }
