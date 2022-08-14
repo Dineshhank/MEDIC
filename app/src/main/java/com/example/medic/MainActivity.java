f77ac47b60ea76567fb242d2a501051a66f2e698
Dineshhank <babud2000@gmail.com>
2022-08-14 13:53:16 +0530

    consultpage

--- app/src/main/java/com/example/medic/MainActivity.java
+++ app/src/main/java/com/example/medic/MainActivity.java
@@ -9,12 +9,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.ViewFlipper;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button1,button2;
+    ImageView button1, button2;
+    ViewFlipper vflipper;
 
 
     //BottomNavigationView navigationView;
@@ -22,19 +27,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         setContentView(activity_main);
-       this.button1=(Button) findViewById(R.id.button3);
-       this.button2=(Button) findViewById(R.id.button3);
+        setContentView(activity_main);
+        this.button1 = (ImageView) findViewById(R.id.button3);
+        this.button2 = (ImageView) findViewById(R.id.button4);
 
-       button1.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               Intent intent = new Intent(MainActivity.this, general.class);
-               startActivity(intent);
-               finish();
-           }
-       });
+        Spinner myspinner = (Spinner) findViewById(R.id.spinner);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, general.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Appoinment.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ArrayAdapter<String> myadapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner.setAdapter(myadapter);
 
 
+        int images[] = {R.drawable.slide2, R.drawable.slide3, R.drawable.slide1,R.drawable.banner1};
+        vflipper = findViewById(R.id.flippernew);
+
+        for (int image : images) {
+            flipperImages(image);
+
+        }
+    }
+
+    public void flipperImages(int image) {
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource(image);
+
+        vflipper.addView(imageView);
+        vflipper.setFlipInterval(3000);
+        vflipper.setAutoStart(true);
+
+        vflipper.setInAnimation(this, android.R.anim.slide_in_left);
+        vflipper.setOutAnimation(this, android.R.anim.slide_out_right);
     }
 }
--- app/src/main/java/com/example/medic/consultnow.java
+++ app/src/main/java/com/example/medic/consultnow.java
@@ -2,18 +2,26 @@ package com.example.medic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class consultnow extends AppCompatActivity {
     ViewFlipper vflipper;
+
+    Button closeButton;
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consultnow);
 
-        int images[] ={R.drawable.img1, R.drawable.medi1, R.drawable.loginbg};
+        int images[] ={R.drawable.banner1, R.drawable.banner2, R.drawable.banner1};
         vflipper = findViewById(R.id.flipper);
 
         for(int image: images){
@@ -31,6 +39,41 @@ public class consultnow extends AppCompatActivity {
 
         vflipper.setInAnimation(this, android.R.anim.slide_in_left);
         vflipper.setOutAnimation(this, android.R.anim.slide_out_right);
+
+        closeButton = (Button) findViewById(R.id.button);
+        builder = new AlertDialog.Builder(this);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Uncomment the below code to Set the message and title from the strings.xml file
+                builder.setMessage(R.string.dialog_message) .setTitle(R.string.dialog_title);
+
+                //Setting message manually and performing action on button click
+                builder.setMessage("Are you sure want to continue to payment")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                finish();
+                                Toast.makeText(getApplicationContext(),"you choose yes action for alertbox",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //  Action for 'NO' Button
+                                dialog.cancel();
+                                Toast.makeText(getApplicationContext(),"you choose no action for alertbox",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                //Creating dialog box
+                AlertDialog alert = builder.create();
+                //Setting the title manually
+                alert.setTitle("Consult Now");
+                alert.show();
+            }
+        });
 
     }
 }
