f0209c29be999e4c1691f788bff9b84acdeb8f4c
DHEENU <dineshkanna.s00@gmail.com>
2022-08-21 13:17:25 +0530

    pay

--- app/src/main/java/com/example/medic/payment.java
+++ app/src/main/java/com/example/medic/payment.java
@@ -1,14 +1,65 @@
 package com.example.medic;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class payment extends AppCompatActivity {
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class payment extends AppCompatActivity implements PaymentResultListener {
+    Button btPay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        btPay = findViewById(R.id.pay);
+        String sAmount="100";
+        int amount= Math.round(Float.parseFloat(sAmount)*100);
+        btPay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Checkout checkout=new Checkout();
+                checkout.setKeyID("rzp_test_PuWsKZ79BPFMdG");
+                checkout.setImage(com.razorpay.R.drawable.rzp_logo);
+                JSONObject object=new JSONObject();
+               try {
+                   object.put("name","dheenu");
+                   object.put("description","Test Payment");
+                   object.put("theme.color","0093DD");
+                   object.put("currency","INR");
+                   object.put("amount",amount);
+                   object.put("prefill.contact","9095353249");
+                   object.put("prefill.email","dineshkanna.s00@gmail.com");
+                   checkout.open(payment.this,object);
+               }catch (JSONException e){
+                   e.printStackTrace();
+               }
+
+            }
+        });
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("Payment ID");
+        builder.setMessage(s);
+        builder.show();
+
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(getApplicationContext()
+        ,s,Toast.LENGTH_SHORT).show();
+
     }
 }
